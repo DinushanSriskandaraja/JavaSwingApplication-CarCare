0f144ed62b5aaa09e2b8ac93d2966824d3353f2f
Sri Dinu <sridinu03@gmail.com>
2024-01-14 01:11:19 +0530

    converted to maven

---
+++ src/View/Dashboard.java
@@ -0,0 +1,36 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Dashboard{
+    private JButton supplierButton;
+    private JButton customerButton;
+    private JButton inventoryButton;
+    private JPanel btnCustomer;
+
+public Dashboard(){
+
+    inventoryButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+    customerButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+    supplierButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
+
+}
