ac40772819dc0799d3cf74d7cb966b92a7251f94
Mohammed <farhanrefai20@gmail.com>
2024-01-14 12:31:03 +0530

    made some changes

--- src/main/java/Model/DatabaseManager.java
+++ src/main/java/Model/DatabaseManager.java
@@ -1,3 +1,5 @@
+package Model;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/View/Dashboard.java
+++ src/main/java/View/Dashboard.java
@@ -1,8 +1,19 @@
 package View;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Dashboard {
     private JButton button1;
     private JPanel panel1;
+
+    public Dashboard() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
